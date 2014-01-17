bfb0bc3d8bd0f453436229a795a055aa95792f79
xulingjue <lj.ac@outlook.com>
2014-01-17 12:00:59 +0800

    struts 可正常运行，已加好lib包

--- WebContent/addQuestion.jsp
+++ WebContent/addQuestion.jsp
@@ -16,6 +16,5 @@
 </form>
 
 
-
 </body>
 </html>
--- src/com/xulingjue/traffictestserver/servlet/QuestionAddAction.java
+++ src/com/xulingjue/traffictestserver/servlet/QuestionAddAction.java
@@ -5,27 +5,10 @@ import java.util.Map;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class QuestionAddAction extends ActionSupport{
-	private String content;
-	private String options[];
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String[] getOptions() {
-		return options;
-	}
-	public void setOptions(String[] options) {
-		this.options = options;
-	}
-	
+
 	//处理用户请求的方法
 	public String execute(){
-		for(int i=0;i<this.options.length;i++){
-			System.out.println(this.options[i]);
-		}
-		
+		System.out.println("hello");
 		return SUCCESS;
 	}
 }
